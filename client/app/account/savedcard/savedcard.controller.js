6df72aa6f98df5144f72b4bb66ce1f00af9bb009
Harsh Raval <harshraval9@gmail.com>
2014-12-02 18:20:30 +0530

    add card form. list of saved card.

--- client/app/account/savedcard/savedcard.controller.js
+++ client/app/account/savedcard/savedcard.controller.js
@@ -5,5 +5,26 @@ angular.module('zesty')
     function ($scope) {
 
   $scope.siteName = 'Zesty';
+  $scope.firstTime = false;
+
+  $scope.cards = [{
+    number: '5432-xxxx-xxxx-xx65',
+    type: 1,
+    name: 'Harsh Raval',
+    expiryDate: '10/19',
+    label: 'SBI card'
+  }, {
+    number: '5432-xxxx-xxxx-xx65',
+    type: 2,
+    name: 'Harsh Raval',
+    expiryDate: '10/19',
+    label: 'SBI card'
+  }, {
+    number: '5432-xxxx-xxxx-xx65',
+    type: 0,
+    name: 'Harsh Raval',
+    expiryDate: '10/19',
+    label: 'SBI card'
+  }];
 
 }]);
