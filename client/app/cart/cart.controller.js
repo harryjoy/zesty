bf0e86c8c97d560dd13a3aa85553570050a86b63
Harsh Raval <harshraval9@gmail.com>
2014-12-03 12:32:30 +0530

    Redirect to auth or checkout page based on login status from cart page.

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('zesty')
-  .controller('CartCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('CartCtrl', ['$scope', 'Auth', '$location', function ($scope, Auth, $location) {
+    $scope.checkout = function() {
+      Auth.isLoggedInAsync(function(loggedIn) {
+        if (!loggedIn) {
+          $location.path('/auth');
+        } else {
+          $location.path('/checkout');
+        }
+      });
+    };
+  }]);
