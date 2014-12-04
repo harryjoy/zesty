043a2df750ccfe9f04931986ce713f3c961bc140
Harsh Raval <harshraval9@gmail.com>
2014-12-04 18:38:31 +0530

    Updated change password UI. Added error and success alert messages on chanhe password page.

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -1,21 +1,32 @@
 'use strict';
 
 angular.module('zesty')
-  .controller('SettingsCtrl', ['$scope', 'User', 'Auth', function ($scope, User, Auth) {
+  .controller('SettingsCtrl', ['$scope', 'User', 'Auth', '$timeout',
+    function ($scope, User, Auth, $timeout) {
     $scope.errors = {};
 
     $scope.changePassword = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
+        if ($scope.user.newPassword === $scope.user.confirmPassword) {
+          Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
+          .then( function() {
+            $scope.message = 'Password successfully changed.';
+            $timeout(function() {
+              $scope.message = '';
+            }, 3000);
+          })
+          .catch( function() {
+            form.password.$setValidity('mongoose', false);
+            $scope.errors.other = 'Incorrect current password, please try again.';
+            $scope.message = '';
+          });
+        } else {
+          form.newPassword.$setValidity('mongoose', false);
+          form.confirmPassword.$setValidity('mongoose', false);
+          $scope.errors.other = 'Entered new password and confirm password do not match.';
           $scope.message = '';
-        });
+        }
       }
 		};
 
