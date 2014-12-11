fe395b5c8bae6858f3ecfdad7b9e94f3ae757dcc
Harsh Raval <harshraval9@gmail.com>
2014-12-11 13:00:30 +0530

    Integrated favorites api on category item list page.

--- client/app/category/category.controller.js
+++ client/app/category/category.controller.js
@@ -1,16 +1,24 @@
 'use strict';
 
 angular.module('zesty')
-  .controller('CategoryCtrl', ['$scope', '$stateParams', 'CategoryServ',
-	  function ($scope, $stateParams, CategoryServ) {
-		var categoryId = $stateParams.id;
-    CategoryServ.get({id: categoryId}).$promise.then(function(category) {
-      $scope.category = category;
-    });
-    $scope.items = [];
-    CategoryServ.items({id: categoryId}).$promise.then(function(items) {
+  .controller('CategoryCtrl', ['$scope', '$stateParams', 'CategoryServ', 'Auth', 'FavoriteServ',
+    function ($scope, $stateParams, CategoryServ, Auth, FavoriteServ) {
+
+  var categoryId = $stateParams.id;
+  CategoryServ.get({id: categoryId}).$promise.then(function(category) {
+    $scope.category = category;
+  });
+  $scope.items = [];
+  $scope.customerId = '';
+  $scope.loggedIn = Auth.isLoggedIn();
+  if ($scope.loggedIn) {
+    $scope.customerId = $scope.getCurrentUser()._id;
+  }
+  CategoryServ.items({id: categoryId}).$promise.then(function(items) {
+    if (items && items.length > 0) {
       $.each(items, function (key, item) {
         $scope.items.push({
+          '_id': item._id,
           'link': '/product/' + item._id,
           'title': item.title,
           'price': item.currency + ' ' + item.price,
@@ -21,5 +29,34 @@ angular.module('zesty')
           'rating': item.rating ? item.rating : 0
         });
       });
-    });
-  }]);
+      if ($scope.loggedIn) {
+        $scope.checkForProductFav();
+      }
+    }
+  });
+  $scope.checkForProductFav = function() {
+    if ($scope.items && $scope.items.length > 0) {
+      var productIds = [];
+      $.each($scope.items, function(k, item) {
+        productIds.push(item._id);
+      });
+      FavoriteServ.checkForProducts({
+        productId: productIds
+      }, function (favs) {
+        if (favs && favs.length > 0) {
+          $.each($scope.items, function(k, item) {
+            var myFav = false;
+            $.each(favs, function(k, fav) {
+              if (fav.productId === item._id) {
+                myFav = true;
+              }
+            });
+            item.myFav = myFav;
+          });
+        }
+      }, function (err) {
+        console.log(err);
+      });
+    }
+  };
+}]);
